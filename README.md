00f1aee9ce7002f43f1bc15b22e1576ec0b51370
Ultron <maryan.zozulia@gmail.com>
2019-01-27 13:42:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Example Package
+
+This is a simple example package. You can install it with pip install command
+
+```console
+pip install git+ssh://git@gitlab.cloudferro.com:65522/mzozulia/utils.git
+```
+
---
+++ example_pkg/wrapper.py
@@ -0,0 +1,28 @@
+def arrows_wrapper(string):
+    print("↧" * len(string))
+    print(string)
+    print("↥" * len(string))
+
+
+def stars_wrapper(string):
+    print("*" * len(string))
+    print(string)
+    print("*" * len(string))
+
+
+def lines_wrapper(string):
+    print("-" * len(string))
+    print(string)
+    print("-" * len(string))
+
+
+def hashtags_wrapper(string):
+    print("#" * len(string))
+    print(string)
+    print("#" * len(string))
+
+
+def tildes_wrapper(string):
+    print("~" * len(string))
+    print(string)
+    print("~" * len(string))
